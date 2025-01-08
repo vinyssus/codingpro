ec9d059438fdd37b0abf477a8ba9e30bfe2967d1
vinyssus <vinyssusrodolphe@gmail.com>
2025-01-08 02:08:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import "./App.css";
+import TopNavbar from "./template/structure/navbar/TopNavbar";
+
+function App() {
+  return (
+    <div>
+     <TopNavbar />
+    </div>
+  );
+}
+
+export default App;
